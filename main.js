9a46487a8975bea4938aa8f1443d5f1b747a9239
Mohammed Asker <zetoanime@gmail.com>
2019-12-11 18:12:31 +0300

    added menu bar animations

---
+++ main.js
@@ -0,0 +1,10 @@
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
+// toggle drop down menu
+navBarToggle.addEventListener("click", function() {
+mainNav.classList.toggle("active");
+});
+// toggle the menu bar
+function myFunction(x) {
+			x.classList.toggle("change");
+		}
